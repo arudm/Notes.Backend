e049beb79f2e729a64f523026ba65c7a1c13548d
arudm <arustdmit@gmail.com>
2022-12-20 17:21:32 +0300

    + Был создан интерфейс INotesDbContext для БД + Был создан класс DbInitializer для проверки создания БД + Был создан класс контекста NotesDbContext + Была создана конфигруация NoteConfiguration сущности Note

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+
+        // Представляет коллекцию всех сущностей в контексте или может запрашиваться из БД заданного типа
+        DbSet<Note> Notes { get; set; }
+
+        // Дублируем сигнатуру ради удобства из DbContext EntityFramework-а
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
+ 
---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    /// <summary>
+    /// Нужен для проверки создания БД  
+    /// </summary>
+    public class DbInitializer
+    {
+        /// <summary>
+        /// Проверяет и инициализирует БД, если ее не существует
+        /// </summary>
+        /// <param name="context"></param>
+        public static void Initialize(NotesDbContext context) 
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
